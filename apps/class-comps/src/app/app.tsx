9f5dcb7408566cbf061ad20d21bd4e92d1fae033
Harsha Narayana <itsharshanarayana@gmail.com>
2022-08-17 22:17:52 -0400

    Additional changes

--- apps/class-comps/src/app/app.tsx
+++ apps/class-comps/src/app/app.tsx
@@ -33,7 +33,7 @@ class App extends React.Component<AppProps, AppState> {
     );
   }
 
-  override render() {
+  renderContent = () => {
 
     if (this.state.errorMessage && !this.state.lat) return <div>Error: { this.state.errorMessage }</div>
 
@@ -42,6 +42,23 @@ class App extends React.Component<AppProps, AppState> {
     }
 
     return <Spinner message="Please accept location request!" />
+  }
+
+  override render() {
+
+    /*if (this.state.errorMessage && !this.state.lat) return <div>Error: { this.state.errorMessage }</div>
+
+    if (!this.state.errorMessage && this.state.lat) {
+      return <SeasonDisplay month={new Date().getMonth()} latitude={this.state.lat} />
+    }
+
+    return <Spinner message="Please accept location request!" />*/
+
+    return (
+      <div>
+        {this.renderContent()}
+      </div>
+    );
 
   }
 
