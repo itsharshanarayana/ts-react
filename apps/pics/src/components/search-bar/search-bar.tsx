83f15fa7ba5715e6170c04a5695062f09d0f37d1
Harsha Narayana <itsharshanarayana@gmail.com>
2022-08-18 22:18:48 -0400

    Form element

--- apps/pics/src/components/search-bar/search-bar.tsx
+++ apps/pics/src/components/search-bar/search-bar.tsx
@@ -10,16 +10,32 @@ export interface SearchBarState {
 }
 
 class SearchBar extends React.Component<SearchBarProps, SearchBarState> {
+
   override state: SearchBarState = {term: ''};
+
+
+  onInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({ term: event.target.value });
+  }
+
+  onFormSubmit = (event:  React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log("term data: ", this.state.term);
+  }
 
 
   override render() {
     return (
       <div className="ui segment">
-        <form className="ui form">
+        <form className="ui form"
+              onSubmit={this.onFormSubmit}>
           <div className="field">
             <label htmlFor="term">Image Search</label>
-            <input id="term" type="text"/>
+            <input id="term"
+                   type="text"
+                   onChange={this.onInputChange}
+                   value={this.state.term}  // This makes the element controlled element - controlled by react.
+            />
           </div>
         </form>
       </div>
