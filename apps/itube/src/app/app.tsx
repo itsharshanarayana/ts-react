e0b6f8c093f28f979b0f502dd298ed07fea6ea85
Harsha Narayana <itsharshanarayana@gmail.com>
2022-08-19 23:29:45 -0400

    SearchBar component changes

--- apps/itube/src/app/app.tsx
+++ apps/itube/src/app/app.tsx
@@ -4,16 +4,22 @@ import React from "react";
 
 import {AppProps} from "../types/app-types";
 import {AppState} from "../types/app-types";
+import SearchBar from "../components/search-bar/search-bar";
 
 class App extends React.Component<AppProps, AppState> {
 
   override render() {
     return (
-      <>
-        <h1>ITube Application</h1>
-        <div/>
-      </>
+      <div>
+        <SearchBar
+          onSearchSubmit={this.onSearchSubmit}
+        />
+      </div>
     );
+  }
+
+  onSearchSubmit = (searchTerm: string) => {
+    console.log("Search term:", searchTerm);
   }
 }
 
--- apps/itube/src/components/search-bar/search-bar.tsx
+++ apps/itube/src/components/search-bar/search-bar.tsx
@@ -1,14 +1,45 @@
 import styles from './search-bar.module.css';
+import React from "react";
+import {Box, FormControl, FormLabel, TextField} from "@mui/material";
 
 /* eslint-disable-next-line */
-export interface SearchBarProps {}
+interface SearchBarProps {
+  onSearchSubmit: (searchTerm: string) => void;
+}
 
-export function SearchBar(props: SearchBarProps) {
-  return (
-    <div className={styles['container']}>
-      <h1>Welcome to SearchBar!</h1>
-    </div>
-  );
+interface SearchBarState {
+  searchTerm: string;
+}
+
+class SearchBar extends React.Component<SearchBarProps, SearchBarState> {
+
+  override state: SearchBarState = { searchTerm: '' };
+  override render() {
+    return (
+      <form onSubmit={this.onFormSubmit}>
+        <FormControl>
+          <Box sx={{ width: 500, maxWidth: '100%', }}>
+            <TextField fullWidth
+                       label="Video Search"
+                       id="fullWidth"
+                       onChange={this.onTextFieldChange}
+                       value={this.state.searchTerm}
+            />
+          </Box>
+        </FormControl>
+      </form>
+    );
+  }
+
+  onTextFieldChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({searchTerm: event.target.value });
+  }
+
+  onFormSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    this.props.onSearchSubmit(this.state.searchTerm);
+  }
+
 }
 
 export default SearchBar;
