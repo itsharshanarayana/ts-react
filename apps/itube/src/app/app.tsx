ecbc1bdfe38558c70aa6af43be2e7a0dd04c07ea
Harsha Narayana <itsharshanarayana@gmail.com>
2022-08-19 22:35:28 -0400

    New app - itube

---
+++ apps/itube/src/app/app.tsx
@@ -0,0 +1,13 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from './app.module.css';
+
+export function App() {
+  return (
+    <>
+      <h1>ITube Application</h1>
+      <div />
+    </>
+  );
+}
+
+export default App;
