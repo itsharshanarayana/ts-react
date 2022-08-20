84a7ea6aa025dd94f53dd7fdeb03143f07997c06
Harsha Narayana <itsharshanarayana@gmail.com>
2022-08-19 23:38:02 -0400

    sample change

--- apps/pics/src/api/unsplash.ts
+++ apps/pics/src/api/unsplash.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 export default axios.create({
     baseURL: 'https://api.unsplash.com',
     headers: {
-      Authorization: 'Client-ID 6_s3vY00GqZsb1D_4mYqFUgKJ62zvH8-bXhUD1JOnng'
+      Authorization: 'Client-ID 6_s3vY00GqZsb1D_4mYqFUgKJ62zvH8'
     }
   }
 );
