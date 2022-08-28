986c58a56b612515129fcb647bcc6d6b57bb9dca
Harsha Narayana <itsharshanarayana@gmail.com>
2022-08-27 22:29:48 -0400

    Changes using useMutation hook

--- apps/blog-em-ipsum/src/components/post-detail/post-detail.tsx
+++ apps/blog-em-ipsum/src/components/post-detail/post-detail.tsx
@@ -1,9 +1,10 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+
 import styles from './post-detail.module.css';
 import React, {useState} from "react";
 import {Post} from "../posts/posts";
-import {useQuery} from "react-query";
-import {fetchComments} from "../../posts-service";
-import Comments from "../comments/comments";
+import {useMutation, useQuery} from "react-query";
+import {deletePost, fetchComments, updatePost} from "../../posts-service";
 
 /* eslint-disable-next-line */
 export interface PostDetailProps {
@@ -35,6 +36,14 @@ const PostDetail: React.FC<PostDetailProps> = ({post}) => {
     }
   );
 
+  const deleteMutation = useMutation(
+    (postId: number) => deletePost(postId)
+  );
+  const postMutation = useMutation(
+    "posts",
+    (postId: number) => updatePost(postId)
+  );
+
   if (isLoading) return <h4>Loading ...</h4>;
 
   if (isError) {
@@ -44,10 +53,51 @@ const PostDetail: React.FC<PostDetailProps> = ({post}) => {
     return <h4>Error: {errorStr}</h4>;
   }
 
+
   return (
     <>
       <h3 style={{ color: "blue" }}>{post.title}</h3>
-      <button>Delete</button> <button>Update title</button>
+
+      <button onClick={
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        () => deleteMutation.mutate(post.id)
+      }>Delete</button>
+      {
+        deleteMutation.isError && (
+          <p style={{ color: 'red'}}>Error deleting the post</p>
+        )
+      }
+
+      {
+        deleteMutation.isLoading && (
+          <p style={{ color: 'purple'}}>Deleting the post ...</p>
+        )
+      }
+      {
+        deleteMutation.isSuccess && (
+          <p style={{ color: 'green'}}>Deleting the post was successful!</p>
+        )
+      }
+
+      <button
+        onClick={() => postMutation.mutate(post.id)}
+      >Update title</button>
+      {
+        postMutation.isError && (
+          <p style={{ color: 'red' }}>Error updating the post!</p>
+        )
+      }
+      {
+        postMutation.isLoading && (
+          <p style={{ color: 'purple' }}>Updating the post ...</p>
+        )
+      }
+      {
+        postMutation.isSuccess && (
+          <p style={{ color: 'green' }}>Post updated successfully!</p>
+        )
+      }
       <p>{post.body}</p>
       <h4>Comments</h4>
       {data?.map((comment) => (
